413bd067aa60d3b289d084c55aad1be1f222dd4f
funksem <holzhaus16@googlemail.com>
2014-02-03 15:27:35 +0100

    fdgdfg

--- src/main/java/de/funksem/mp3caretaker/App.java
+++ src/main/java/de/funksem/mp3caretaker/App.java
@@ -2,12 +2,14 @@ package de.funksem.mp3caretaker;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
